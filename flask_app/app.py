a389dde81e7c6cd050a466c5e3496e293bc09f3b
Aayush shah <23053738@kiit.ac.in>
2026-07-18 22:09:27 +0530

    add docker workflow

--- flask_app/app.py
+++ flask_app/app.py
@@ -111,7 +111,7 @@ def predict():
     return render_template('index.html', result=int(prediction[0]))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
 
 
 
