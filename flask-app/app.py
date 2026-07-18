2c256d1a0b31de4ee43b1c17409288dfbd90ee8a
Aayush shah <23053738@kiit.ac.in>
2026-07-18 15:03:23 +0530

    add requirements and ci.yaml

--- flask-app/app.py
+++ flask-app/app.py
@@ -35,3 +35,45 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# from fastapi import FastAPI, Request, Form
+# from fastapi.templating import Jinja2Templates
+# from fastapi.responses import HTMLResponse
+# import mlflow
+# import mlflow.pyfunc
+# from preprocessing_utility import normalize_text
+# import pickle
+
+# import dagshub
+
+# dagshub.init(repo_owner='Aayush10671', repo_name='emotion-detection', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/Aayush10671/emotion-detection.mlflow')
+
+# app = FastAPI()
+# templates = Jinja2Templates(directory="templates")
+
+# model_name = "sentiment_classifier"
+# model_version = 2
+# model_uri = f"models:/{model_name}/{model_version}"
+# model = mlflow.pyfunc.load_model(model_uri)
+
+# vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
+
+
+# @app.get('/', response_class=HTMLResponse)
+# def home(request: Request):
+#     return templates.TemplateResponse(
+#         "index.html", {"request": request, "result": None, "text": ""}
+#     )
+
+
+# @app.post('/predict', response_class=HTMLResponse)
+# def predict(request: Request, text: str = Form(...)):
+#     cleaned = normalize_text(text)
+#     features = vectorizer.transform([cleaned])
+#     prediction = model.predict(features)
+#     return templates.TemplateResponse(
+#         "index.html",
+#         {"request": request, "result": int(prediction[0]), "text": text}
+#     )
