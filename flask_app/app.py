930a4f37207c574b4ee7f524679524881cba37ef
Aayush shah <23053738@kiit.ac.in>
2026-07-18 18:02:01 +0530

    add flask_app testing code 7

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,7 +4,74 @@ import mlflow
 from mlflow.tracking import MlflowClient
 import pickle
 import pandas as pd
-from preprocessing_utility import normalize_text
+
+# data preprocessing
+import mlflow
+import pandas as pd
+import mlflow.sklearn
+from sklearn.feature_extraction.text import CountVectorizer , TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier , GradientBoostingClassifier
+from sklearn.metrics import accuracy_score , precision_score , recall_score,f1_score
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
+
+def lemmatization(text):
+    """Lemmatize the text."""
+    lemmatizer = WordNetLemmatizer()
+    text = text.split()
+    text = [lemmatizer.lemmatize(word) for word in text]
+    return " ".join(text)
+
+def remove_stop_words(text):
+    """Remove stop words from the text."""
+    stop_words = set(stopwords.words("english"))
+    text = [word for word in str(text).split() if word not in stop_words]
+    return " ".join(text)
+
+def removing_numbers(text):
+    """Remove numbers from the text."""
+    text = ''.join([char for char in text if not char.isdigit()])
+    return text
+
+def lower_case(text):
+    """Convert text to lower case."""
+    text = text.split()
+    text = [word.lower() for word in text]
+    return " ".join(text)
+
+def removing_punctuations(text):
+    """Remove punctuations from the text."""
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = text.replace('؛', "")
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+def removing_urls(text):
+    """Remove URLs from the text."""
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    return url_pattern.sub(r'', text)
+
+
+def normalize_text(text):
+   
+        text = lower_case(text)
+        text = remove_stop_words(text)
+        text = removing_numbers(text)
+        text = removing_punctuations(text)
+        text = removing_urls(text)
+        text = lemmatization(text)
+        return text
+  
+
+  
 
 app = Flask(__name__)
 
