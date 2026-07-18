7d0caaf804ad60b3be6a50ff94850054fa02e9f3
Aayush shah <23053738@kiit.ac.in>
2026-07-18 17:38:03 +0530

    add flask_app testing code

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,41 +1,50 @@
+# app.py
 from flask import Flask, request, render_template
 import mlflow
-import mlflow.pyfunc
-from preprocessing_utility import normalize_text
+from mlflow.tracking import MlflowClient
 import pickle
-
-import dagshub
-
-dagshub.init(repo_owner='Aayush10671', repo_name='emotion-detection', mlflow=True)
-mlflow.set_tracking_uri('https://dagshub.com/Aayush10671/emotion-detection.mlflow')
+import pandas as pd
+from preprocessing_utility import normalize_text
 
 app = Flask(__name__)
 
-model_name = "sentiment_classifier"
-model_version = 2
-model_uri = f"models:/{model_name}/{model_version}"
-model = mlflow.pyfunc.load_model(model_uri)
+# MLflow setup
+mlflow.set_tracking_uri('https://dagshub.com/Aayush10671/emotion-detection.mlflow')
 
+# Load model
+client = MlflowClient()
+prod_versions = client.get_latest_versions("sentiment_classifier", stages=["Production"])
+
+if prod_versions:
+    model_version = prod_versions[0].version
+else:
+    all_versions = client.search_model_versions("name='sentiment_classifier'")
+    model_version = sorted(all_versions, key=lambda v: int(v.version), reverse=True)[0].version
+
+model = mlflow.pyfunc.load_model(f"models:/sentiment_classifier/{model_version}")
 vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
-
 
 @app.route('/')
 def home():
     return render_template('index.html', result=None)
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     text = request.form['text']
     text = normalize_text(text)
-    features = vectorizer.transform([text])
-    prediction = model.predict(features)
+    
+    # Transform and convert to DataFrame
+    sparse_matrix = vectorizer.transform([text])
+    features_df = pd.DataFrame(
+        sparse_matrix.toarray(),
+        columns=vectorizer.get_feature_names_out()
+    )
+    
+    prediction = model.predict(features_df)
     return render_template('index.html', result=int(prediction[0]))
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 # from fastapi import FastAPI, Request, Form
 # from fastapi.templating import Jinja2Templates
